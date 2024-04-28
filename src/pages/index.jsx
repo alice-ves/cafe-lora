283256c7ecb99e1cf4ed2727675c260469c1ad23
Alice Veselá <alice.hladikova@gmail.com>
2024-04-28 06:52:47 +0200

    aktocvace tlacitka objednavek v hlavnim souboru

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,60 @@
 import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
-import { Header } from '../components/header/header';
-import { Banner } from '../components/banner/banner';
-import { Menu } from '../components/menu/menu';
-import { Gallery } from '../components/gallery/gallery';
-import { Contact } from '../components/contact/contact';
-import { Footer } from '../components/footer/footer';
+import { Header } from '../components/Header/Header';
+import { Banner } from '../components/Banner/Banner';
+import { Menu } from '../components/Menu/Menu';
+import { Gallery } from '../components/Gallery/Gallery';
+import { Contact } from '../components/Contact/Contact';
+import { Footer } from '../components/Footer/Footer';
+
+const resp = await fetch('http://localhost:4000/api/drinks');
+const data = await resp.json();
+const drinks = data.data;
+console.log(drinks);
+
+const handleOrder = async (event) => {
+  event.preventDefault();
+  const drinkId = Number(event.target.dataset.id);
+  console.log(drinkId);
+
+  const resp = await fetch(`http://localhost:4000/api/drinks/${drinkId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify([
+      { op: 'replace', path: '/ordered', value: !drinks[drinkId].ordered },
+    ]),
+  });
+
+  if (resp.ok) {
+    window.location.reload();
+  } else {
+    alert('Objednávka se nezdařila. Zkuste to za chvíli znovu.');
+  }
+};
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
-    <Header></Header>
+    <Header />
     <main>
-      <Banner></Banner>
-      <Menu></Menu>
-      <Gallery></Gallery>
-      <Contact></Contact>
+      <Banner />
+      <Menu drinks={drinks} />
+      <Gallery />
+      <Contact />
     </main>
-    <Footer></Footer>
+    <Footer />
   </div>,
 );
 
-const navBtn = document.querySelector('.nav-btn');
-navBtn.addEventListener('click', () => {
+const handleNav = () => {
   document.querySelector('.rollout-nav').classList.toggle('nav-closed');
-});
+};
+document.querySelector('.nav-btn').addEventListener('click', handleNav);
+document.querySelector('.rollout-nav').addEventListener('click', handleNav);
 
-const rolloutNav = document.querySelector('.rollout-nav');
-rolloutNav.addEventListener('click', () => {
-  document.querySelector('.rollout-nav').classList.add('nav-closed');
+const orderBtn = document.querySelectorAll('.drink__controls');
+orderBtn.forEach((btn) => {
+  btn.addEventListener('submit', handleOrder);
 });
